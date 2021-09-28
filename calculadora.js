5cac2099051fe91c0f0d4d6acbbd6c526ddc8cc0
brenofelixdasilva <85354255+brenofelixdasilva@users.noreply.github.com>
2021-09-28 20:25:49 -0300

    segundo commit

--- calculadora.js
+++ calculadora.js
@@ -1,17 +1,21 @@
-const sete;
-const
-const 
-const 
-const 
-const 
-const 
-const 
-const 
-const 
-const 
-const 
-const 
-const 
-const 
-const 
-const 
+const sete = document.getElementById(sete);
+const oito = document.getElementById(oito);  
+const nove = document.getElementById(nove);
+
+const quatro = document.getElementById(quatro);
+const cinco = document.getElementById(cinco);
+const seis = document.getElementById(seis);
+
+const um = document.getElementById(um);
+const dois = document.getElementById(dois);   
+const tres = document.getElementById(tres);
+
+const zero = document.getElementById(zero);
+
+
+const somar = document.getElementById(somar);
+const dividir = document.getElementById(dividir);
+const subtrair = document.getElementById(subtrair);
+const multiplicar = document.getElementById(multiplicar);
+const igual = document.getElementById(resultado);
+const ponto = document.getElementById(ponto);
